a0091a4151c8eb0a23959e48ef3a00a2e45bc347
mosfetz <nick.lloyd.gill@gmail.com>
2024-09-10 12:18:25 -0600

    HW4 materials

---
+++ docs/hw4.md
@@ -0,0 +1,10 @@
+# Homework 4: Timed Counter VHDL 
+
+## Overview
+This homework investigates creating a timer and verifying its function with a test bench.
+
+## Deliverables
+[screenshot](/assets/hw4_sc.jpg)
+
+
+
---
+++ sim/timed counter/timed_counter.vhd
@@ -0,0 +1,43 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity timed_counter is
+
+generic (clk_period : time;
+	count_time : time);
+
+port (clk : in std_ulogic;
+      enable : in boolean;
+      done : out boolean);
+
+end entity timed_counter;
+
+	
+
+architecture timed_counter_arch of timed_counter is 
+
+	signal Count_Out_int	:	integer range 0 to 10000000;
+	constant COUNTER_LIMIT	:	integer:= (count_time/clk_period);
+
+	begin
+
+	Counter : process (clk, enable)
+		begin
+			if (enable = false) then
+			    Count_Out_int <= 0;
+			    done <= false;
+
+			elsif (rising_edge(clk)) then 
+
+				if (Count_Out_int = COUNTER_LIMIT) then
+				  Count_Out_int <= 0;
+				  done <= true;
+				else
+				  Count_Out_int <= Count_Out_int + 1;
+				  done <= false;
+				end if;
+
+			end if;
+
+		end process;
+end architecture; 
